43de38e33f928a5a88afd3881a92ffcba586e061
Sawyer Rice <sawyerrice@ucsb.edu>
2023-11-28 14:56:52 -0800

    new main

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,22 @@
 //class to handle dynamic shader loading
-#include "shaderprog.h"
+//#include "shaderprog.h"
 //class which represents game objects
-#include "entity.h"
+//#include "entity.h"
 //class which handles rendering of entities
-#include "renderer.h"
-#include "handler.h"
+//#include "renderer.h"
+//#include "handler.h"
 //OpenGL function loading
 //window and input handling
 #include <GL/glut.h> //i think it goes like this
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 // Standard Headers
-#include <iostream>
-#include <vector>
-#include <utility>
+//#include <iostream>
+//#include <vector>
+//#include <utility>
 
+/*
 void frame_buffer_size_callback(GLFWwindow* window, int width, int height) {
     glViewport(0, 0, width, height);
 }
@@ -71,4 +75,56 @@ int main(int argc, char * argv[]) {
         glfwPollEvents();
     }
     glfwTerminate();
+}
+*/
+
+
+
+int window2 = 0, window = 0, width = 400, height = 400;
+
+void display(void)
+{
+    glClearColor(0.0, 1.0, 1.0, 1.0);
+    glClear(GL_COLOR_BUFFER_BIT);
+    glLoadIdentity();
+    printf("display1\n");
+    glFlush();
+}
+
+void display2(void)
+{
+    glClearColor(1.0, 0.0, 0.0, 1.0);
+    glClear(GL_COLOR_BUFFER_BIT);
+    glLoadIdentity();
+    printf("display2\n");
+    glFlush();
+}
+
+void reshape (int w, int h)
+{
+    glViewport(0,0,(GLsizei)w,(GLsizei)h);
+    glutPostRedisplay();
+}
+
+int main(int argc, char **argv)
+{
+    // Initialization stuff
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_RGB);
+    glutInitWindowSize(width, height);
+
+    // Create  window main
+    window = glutCreateWindow("Window 1");
+    glutDisplayFunc(display);
+    glutReshapeFunc(reshape);
+    glutInitWindowPosition(100,100);
+
+    // Create second window
+    window2 = glutCreateWindow("Window 2");
+    glutDisplayFunc(display2);
+    glutReshapeFunc(reshape);
+
+    // Enter Glut Main Loop and wait for events
+    glutMainLoop();
+return 0;
 }
