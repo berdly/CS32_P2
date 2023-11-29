58b08c183fe82bd6b4274d0dc6912801e06283da
Jakob Pinedo <jakobpinedo@ucsb.edu>
2023-11-29 06:12:55 -0800

    test

--- src/main.cpp
+++ src/main.cpp
@@ -52,7 +52,7 @@ int main(int argc, char * argv[]) {
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    GLFWwindow* mwindow{ glfwCreateWindow(800, 600, "CS32 Project 2", glfwGetPrimaryMonitor(), nullptr) };
+    GLFWwindow* mwindow{ glfwCreateWindow(800, 600, "CS32 Project 2", /*glfwGetPrimaryMonitor()*/ nullptr, nullptr) };
     if (!mwindow) {
         std::cout << "Couldn't open window.";
     }
